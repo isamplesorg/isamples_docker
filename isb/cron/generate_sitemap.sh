60f81fbbdde49f38b9878e17a3af51461442e51e
Danny Mandel <danny.mandel@gmail.com>
2021-11-19 10:38:15 -0700

    Update paths, etc.

--- isb/cron/generate_sitemap.sh
+++ isb/cron/generate_sitemap.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
-cd /home/isamples/isamples_inabox
-source /home/isamples/.virtualenvs/isb/bin/activate
-
+declare ROOT=/app
+export PYTHONPATH=$ROOT
+cd $ROOT
+mkdir $ROOT/sitemaps
+mkdir /var/log/isamples
 declare DATE=$(date "+%Y-%m-%d")
-declare OUTPUT_DIR="/home/isamples/sitemaps/$DATE/sitemaps"
+declare OUTPUT_DIR="$ROOT/$DATE/sitemaps"
 echo "Attempting to remove sitemap directory output at $OUTPUT_DIR, ok if this fails"
 rm -rf $OUTPUT_DIR
 echo "Creating sitemap directory output at $OUTPUT_DIR"
 mkdir -p $OUTPUT_DIR
 echo "Invoking sitemap creation script"
-python scripts/generate_things_sitemap.py -h "https://mars.cyverse.org" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
+python scripts/generate_things_sitemap.py -h "http://henry.cyverse.org:8000" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
 echo "Removing old sitemaps symlink"
-rm -rf /home/isamples/isamples_inabox/isb_web/sitemaps
+rm -rf $ROOT/isb_web/sitemaps
 echo "Creating new sitemaps symlink"
-ln -s $OUTPUT_DIR /home/isamples/isamples_inabox/isb_web/
+ln -s $OUTPUT_DIR $ROOT/isb_web/
--- isb/cron/geome_update.sh
+++ isb/cron/geome_update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /home/isamples/isamples_inabox
-source /home/isamples/.virtualenvs/isb/bin/activate
+cd /app
 echo "Going to invoke GEOME update load"
 python scripts/geome_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_load.txt
 echo "Going to invoke GEOME solr update"
 python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
 echo "Manually refreshing solr"
-curl "http://localhost:8983/solr/isb_core_records/update?commit=true"
+curl "http://solr:8983/solr/isb_core_records/update?commit=true"
