eedb55a84372e054cd949be20d68bbdc79d0a3e0
Danny Mandel <danny.mandel@gmail.com>
2021-12-01 10:22:29 -0700

    Get uvicorn root path passed in from environment variable built during Docker image construction

--- isb/isb_container_startup.sh
+++ isb/isb_container_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/sbin/service cron start 
-uvicorn isb_web.main:app --host 0.0.0.0 --port 8000
+uvicorn isb_web.main:app --host 0.0.0.0 --port 8000 --root-path /$ISB_UVICORN_ROOT_PATH
