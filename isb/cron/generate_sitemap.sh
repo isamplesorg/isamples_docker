0e68f1888912d0131acc63b1de7b309c5cd0cf50
Danny Mandel <danny.mandel@gmail.com>
2021-12-01 12:34:03 -0700

    Use the sitemap prefix from the environment variable, which comes from docker-compose

--- isb/cron/generate_sitemap.sh
+++ isb/cron/generate_sitemap.sh
@@ -10,8 +10,8 @@ echo "Attempting to remove sitemap directory output at $OUTPUT_DIR, ok if this f
 rm -rf $OUTPUT_DIR
 echo "Creating sitemap directory output at $OUTPUT_DIR"
 mkdir -p $OUTPUT_DIR
-echo "Invoking sitemap creation script"
-python scripts/generate_things_sitemap.py -h "http://henry.cyverse.org:8000" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
+echo "Invoking sitemap creation script, using sitemap prefix of $ISB_SITEMAP_PREFIX"
+python scripts/generate_things_sitemap.py -h "$ISB_SITEMAP_PREFIX" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
 echo "Removing old sitemaps symlink"
 rm -rf $ROOT/isb_web/sitemaps
 echo "Creating new sitemaps symlink"
