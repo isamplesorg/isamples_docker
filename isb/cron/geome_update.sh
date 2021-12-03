be07bfb21aa12d423858020300a3a105b7198eb3
Danny Mandel <danny.mandel@gmail.com>
2021-12-03 09:01:44 -0700

    SESAR config

--- isb/cron/geome_update.sh
+++ isb/cron/geome_update.sh
@@ -3,8 +3,8 @@
 cd /app
 export PYTHONPATH=/app
 echo "Going to invoke GEOME update load"
-python scripts/geome_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_load.txt
+/usr/local/bin/python scripts/geome_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_load.txt
 echo "Going to invoke GEOME solr update"
-python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
+/usr/local/bin/python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
 echo "Manually refreshing solr"
 curl "http://solr:8983/solr/isb_core_records/update?commit=true"
--- isb/cron/sesar_update.sh
+++ isb/cron/sesar_update.sh
@@ -3,8 +3,8 @@
 cd /app
 export PYTHONPATH=/app
 echo "Going to invoke SESAR update load"
-python scripts/sesar_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_load.txt
+/usr/local/bin/python scripts/sesar_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_load.txt
 echo "Going to invoke SESAR solr update"
-python scripts/sesar_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_solr.txt
+/usr/local/bin/python scripts/sesar_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_solr.txt
 echo "Manually refreshing solr"
 curl "http://solr:8983/solr/isb_core_records/update?commit=true"
