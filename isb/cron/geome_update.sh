0f80fba727df5afc7c61dcd6d2570a03c184dc11
Danny Mandel <danny.mandel@gmail.com>
2022-04-27 14:17:14 -0700

    Fix elevate paths

--- isb/cron/geome_update.sh
+++ isb/cron/geome_update.sh
@@ -7,7 +7,7 @@ echo "Going to invoke GEOME update load"
 echo "Going to populate points for any new h3 values"
 /usr/local/bin/python scripts/migrations/populate_points_for_h3.py --config ./isb.cfg  >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_points.txt
 echo "Going to fetch heights for any points without height values"
-node elevate.js -d "postgresql+psycopg2://isb_writer:isamplesinabox@db/isb_1" -k "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzRhMWQ2MS04YjQ2LTQxY2YtYjAwMi1mYWQ5YTcyYjE4MzQiLCJpZCI6OTA5NDAsImlhdCI6MTY1MDY0OTQ3MH0.9SO-QGYpjXAOYjj52guTmFJsxRGREekFADUw19hSKB0" >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_elevate.txt
+node elevate/elevate.js -d "postgresql+psycopg2://isb_writer:isamplesinabox@db/isb_1" -k "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzRhMWQ2MS04YjQ2LTQxY2YtYjAwMi1mYWQ5YTcyYjE4MzQiLCJpZCI6OTA5NDAsImlhdCI6MTY1MDY0OTQ3MH0.9SO-QGYpjXAOYjj52guTmFJsxRGREekFADUw19hSKB0" >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_elevate.txt
 echo "Going to invoke GEOME solr update"
 /usr/local/bin/python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
 echo "Manually refreshing solr"
