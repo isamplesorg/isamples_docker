4e465afc65d9718d038735704ea2fed04bcd206f
Danny Mandel <danny.mandel@gmail.com>
2022-04-27 13:47:29 -0700

    Hook up new scripts in cron jobs

--- isb/cron/isamples_central_update.sh
+++ isb/cron/isamples_central_update.sh
@@ -5,21 +5,26 @@ export PYTHONPATH=/app
 
 echo "Going to invoke OpenContext sitemap ingest"
 /usr/local/bin/python scripts/consume_sitemaps.py -u https://henry.cyverse.org/opencontext/sitemaps/sitemap-index.xml -a OPENCONTEXT >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_sitemap.txt
-echo "Going to invoke OpenContext solr index rebuild"
-/usr/local/bin/python scripts/opencontext_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_solr.txt
-
 echo "Going to invoke GEOME sitemap ingest"
 /usr/local/bin/python scripts/consume_sitemaps.py -u https://henry.cyverse.org/geome/sitemaps/sitemap-index.xml -a GEOME >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_sitemap.txt
-echo "Going to invoke GEOME solr index rebuild"
-/usr/local/bin/python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
-
 echo "Going to invoke SESAR sitemap ingest"
 /usr/local/bin/python scripts/consume_sitemaps.py -u https://henry.cyverse.org/sesar/sitemaps/sitemap-index.xml -a SESAR >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_sitemap.txt
-echo "Going to invoke SESAR solr index rebuild"
-/usr/local/bin/python scripts/sesar_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_solr.txt
-
 echo "Going to invoke Smithsonian sitemap ingest"
 /usr/local/bin/python scripts/consume_sitemaps.py -u https://henry.cyverse.org/smithsonian/sitemaps/sitemap-index.xml -a SMITHSONIAN >& /var/log/isamples/`date "+%Y-%m-%d"`.smithsonian_sitemap.txt
+
+echo "Going to assign h3 values for things missing them"
+/usr/local/bin/python scripts/migrations/populate_h3_for_existing_things.py --config ./isb.cfg  >& /var/log/isamples/`date "+%Y-%m-%d"`.h3.txt
+echo "Going to populate points for any new h3 values"
+/usr/local/bin/python scripts/migrations/populate_points_for_h3.py --config ./isb.cfg >& /var/log/isamples/`date "+%Y-%m-%d"`.points.txt
+echo "Going to fetch heights for any points without height values"
+node elevate.js -d "postgresql+psycopg2://isb_writer:isamplesinabox@db/isb_1" -k "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzRhMWQ2MS04YjQ2LTQxY2YtYjAwMi1mYWQ5YTcyYjE4MzQiLCJpZCI6OTA5NDAsImlhdCI6MTY1MDY0OTQ3MH0.9SO-QGYpjXAOYjj52guTmFJsxRGREekFADUw19hSKB0" >& /var/log/isamples/`date "+%Y-%m-%d"`.elevate.txt
+
+echo "Going to invoke OpenContext solr index rebuild"
+/usr/local/bin/python scripts/opencontext_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_solr.txt
+echo "Going to invoke GEOME solr index rebuild"
+/usr/local/bin/python scripts/geome_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.geome_solr.txt
+echo "Going to invoke SESAR solr index rebuild"
+/usr/local/bin/python scripts/sesar_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.sesar_solr.txt
 echo "Going to invoke Smithsonian solr index rebuild"
 /usr/local/bin/python scripts/smithsonian_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.smithsonian_solr.txt
 
--- isb/cron/opencontext_update.sh
+++ isb/cron/opencontext_update.sh
@@ -4,6 +4,10 @@ cd /app
 export PYTHONPATH=/app
 echo "Going to invoke OpenContext update load"
 /usr/local/bin/python scripts/opencontext_things.py --config ./isb.cfg load -m -1 >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_load.txt
+echo "Going to populate points for any new h3 values"
+/usr/local/bin/python scripts/migrations/populate_points_for_h3.py --config ./isb.cfg >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_points.txt
+echo "Going to fetch heights for any points without height values"
+node elevate.js -d "postgresql+psycopg2://isb_writer:isamplesinabox@db/isb_1" -k "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzRhMWQ2MS04YjQ2LTQxY2YtYjAwMi1mYWQ5YTcyYjE4MzQiLCJpZCI6OTA5NDAsImlhdCI6MTY1MDY0OTQ3MH0.9SO-QGYpjXAOYjj52guTmFJsxRGREekFADUw19hSKB0" >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_elevate.txt
 echo "Going to invoke OpenContext solr update"
 /usr/local/bin/python scripts/opencontext_things.py --config ./isb.cfg populate_isb_core_solr >& /var/log/isamples/`date "+%Y-%m-%d"`.opencontext_solr.txt
 echo "Manually refreshing solr"
