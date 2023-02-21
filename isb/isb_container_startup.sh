d8846a29ef72fed8783463a2b80b82a56d69bdab
Danny Mandel <danny.mandel@gmail.com>
2023-02-21 16:17:24 -0700

    Changes required to use gunicorn instead of uvicorn

--- isb/isb_container_startup.sh
+++ isb/isb_container_startup.sh
@@ -11,4 +11,4 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 /usr/sbin/service cron start
 export PYTHONPATH=/app
 /usr/local/bin/python scripts/create_sitemaps_symlink.py -s /app/sitemaps  -d /app/isb_web/sitemaps
-uvicorn isb_web.main:app --host 0.0.0.0 --port 8000 --root-path /$ISB_UVICORN_ROOT_PATH
+gunicorn --bind=0.0.0.0:8000 -w 16 -k uvicorn.workers.UvicornWorker isb_web.main:app
